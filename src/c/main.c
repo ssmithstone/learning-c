ac26034809c7a482b2418e22eeffd3dbddd4b2c8
Stephen Smithstone <stephen@smithstone.co.uk>
2018-03-17 16:43:30 +1100

    :white_check_mark: String modification
    Trying to modify a char in a string that is defined in the code, is not
    possible you have to use strdup with allocates a new string which can
    then be modified, and since strdup is used malloc we need to use free,
    this can be confirmed by running valgrind to see memory leaks, if you remove the free it shows the leak

--- src/c/main.c
+++ src/c/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
+
+void testStringToLowerCase(char *str);
+void testStringToUpperCase(char *str);
+void testStringToSentanceCase(char * str);
+void testSeparateOnToken(char *str);
+void testTrimLeft(char *str);
+void testTrimRight(char *str);
+void testStringFormattingWithPrintf(char *str);
+void testModifyStringWithNewCharAtIndex(char * str , int index , char newchar);
 
 int main() {
-    printf("Hello, World!\n");
+    char * strptr = "Hello";
+    testStringFormattingWithPrintf(strptr);
+    testModifyStringWithNewCharAtIndex(strptr, 0 , 'h');
+    testStringFormattingWithPrintf(strptr);
     return 0;
+}
+
+void testStringToLowerCase(char * const str){}
+void testStringToUpperCase(char * const str){}
+void testStringToSentanceCase(char * const str){}
+void testSeparateOnToken(char * const  str){}
+void testTrimLeft(char * const  str){}
+void testTrimRight(char * const  str){}
+
+void testModifyStringWithNewCharAtIndex(char * str , int index , char newchar){
+    printf("Old string %s\n" , str);
+    printf("We have to duplicate old string to modify\n");
+    char * dup = strdup(str);
+    printf("dup string %s\n" , dup);
+    dup[index]= newchar;
+    printf("Modified dup string %s\n" , dup);
+    printf("Original string %s\n" , str);
+    free(dup);
+}
+void testStringFormattingWithPrintf(char * str){
+    printf("Printing str %s\n" , str );
 }
